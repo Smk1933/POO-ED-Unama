4b597f85b689e34dba48403b789a2b84a62b480f
Samuel Henrique Gomes de Medeiros <samuelhgg93@gmail.com>
2024-08-26 14:33:16 +0000

    Livraria_Ex

---
+++ Exemplos_Praticos/Livraria.java
@@ -0,0 +1,12 @@
+package livraria;
+
+public class Livro {
+    String titulo;
+    int numeroPaginas;
+    double preco;
+
+    void cadastrar () {
+        System.out.println("Cadastrando o" + titulo)
+    }
+}
+
